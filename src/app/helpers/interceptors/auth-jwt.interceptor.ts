9d6076304ca35a4f626e4ce5c54617a1acd052f1
Benjamín <benjamin.diaze@outlook.com>
2025-03-02 21:50:44 -0300

    Se añade redirección a login si en interceptor no se cuenta con token

--- src/app/helpers/interceptors/auth-jwt.interceptor.ts
+++ src/app/helpers/interceptors/auth-jwt.interceptor.ts
@@ -1,14 +1,21 @@
 import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { AuthSession } from 'aws-amplify/auth';
-import { from, Observable, switchMap } from 'rxjs';
+import { catchError, EMPTY, from, Observable, switchMap } from 'rxjs';
 import { CognitoService } from '../../services/cognito/cognito.service';
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 export const authJwtInterceptor: HttpInterceptorFn = (req:HttpRequest<unknown>, next:HttpHandlerFn) => {
     const cognitoService = inject(CognitoService)
+    const router = inject(Router);
 
     return from(cognitoService.obtenerSesionAuth()).pipe(
         switchMap((session:AuthSession) => {
+            if (!session.tokens) {
+                router.navigate(['/login']);
+                return EMPTY;
+            }
+
             const modifiedReq = req.clone({
                 setHeaders: {
                     Authorization: `Bearer ${session.tokens?.idToken?.toString()}`
