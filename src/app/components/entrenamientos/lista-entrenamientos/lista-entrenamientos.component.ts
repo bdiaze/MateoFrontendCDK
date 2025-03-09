57e3ac08991d3758ddfafdc452191ddd3181d7c6
Benjamín <benjamin.diaze@outlook.com>
2025-03-09 18:10:48 -0300

    Se hacen arreglos en paginación, acortandola a 7 elementos.

--- src/app/components/entrenamientos/lista-entrenamientos/lista-entrenamientos.component.ts
+++ src/app/components/entrenamientos/lista-entrenamientos/lista-entrenamientos.component.ts
@@ -75,8 +75,54 @@ export class ListaEntrenamientosComponent {
         .subscribe({
             next: (response:SalEntrenamiento) => {
                 this.salEntrenamientos = response
-                // Se crean las páginas necesarias...
-                this.paginas = new Array(this.salEntrenamientos.totalPaginas).fill(0).map((_, index) => index + 1);
+                // Se crean las páginas necesarias, para tener solo 7 páginas en la paginación (incluyendo las genéricas)...
+                let paginasCercanas: number[];
+                // Si estamos en una de las 3 primeras páginas, se consideran como cercanas hasta la página 5...
+                if ([1, 2, 3].includes(this.salEntrenamientos.pagina)) {
+                    paginasCercanas = [1, 2, 3, 4, 5];
+                // Si estamos en una de las 3 últimas páginas, se consideran como cercanas hasta las últimas 5 páginas...
+                } else if ([this.salEntrenamientos.totalPaginas - 2, this.salEntrenamientos.totalPaginas - 1, this.salEntrenamientos.totalPaginas].includes(this.salEntrenamientos.pagina)) {
+                    paginasCercanas = [
+                        this.salEntrenamientos.totalPaginas - 4,
+                        this.salEntrenamientos.totalPaginas - 3,
+                        this.salEntrenamientos.totalPaginas - 2,
+                        this.salEntrenamientos.totalPaginas - 1,
+                        this.salEntrenamientos.totalPaginas
+                    ];
+                } else {
+                    paginasCercanas = [this.salEntrenamientos.pagina - 1, this.salEntrenamientos.pagina, this.salEntrenamientos.pagina + 1];
+                }
+                this.paginas = [];
+                for (let i = 1; i <= this.salEntrenamientos.totalPaginas; i++) {
+                    if (paginasCercanas.includes(i)) {
+                        this.paginas.push(i);
+                    }
+                }
+                // Si la primera página no es la número 1, se crea la 1...
+                if (this.paginas.length >= 1 && this.paginas[0] != 1) {
+                    // Pero antes de crear el elemento 1, se valida si el primer elemento es el 2, si no lo es se crea antes el genérico...
+                    if (this.paginas.length >= 2 && this.paginas[0] != 2) {
+                        // Si existe un tercer elemento, se valida si es el número 3, de ser así se agrega el elemento 2 en vez del genérico...
+                        if (this.paginas[0] == 3) {
+                            this.paginas.unshift(2);
+                        } else {
+                            this.paginas.unshift(0);
+                        }
+                    }
+                    this.paginas.unshift(1);
+                }
+                // Si la última página no es la final, se crea la final...
+                if (this.paginas.length >= 1 && this.paginas[this.paginas.length - 1] != this.salEntrenamientos.totalPaginas) {
+                    // Pero antes de crear la final, se valida si el último elemento corresponde a la pénultima página, si no lo es se crea antes el genérico...
+                    if (this.paginas.length >= 2 && this.paginas[this.paginas.length - 1] != this.salEntrenamientos.totalPaginas - 1) {
+                        if (this.paginas[this.paginas.length - 1] == this.salEntrenamientos.totalPaginas - 2) {
+                            this.paginas.push(this.salEntrenamientos.totalPaginas - 1);
+                        } else {
+                            this.paginas.push(0);
+                        }
+                    }
+                    this.paginas.push(this.salEntrenamientos.totalPaginas);
+                }
 
                 // Se ordenan los entrenamientos según la fecha de inicio...
                 this.salEntrenamientos.entrenamientos = this.salEntrenamientos.entrenamientos?.sort((e1, e2) => {
@@ -102,7 +148,6 @@ export class ListaEntrenamientosComponent {
                     }
                     this.entrenamientosPorFechas![this.entrenamientosPorFechas!.length - 1].entrenamientos.push(entrenamiento);
                 });
-                console.log(this.entrenamientosPorFechas);
 
                 this.cargando = false;
             },
